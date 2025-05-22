6d9a97145f98289606beb2421a46a84968f225f1
Gabriel Luís da Silva <79342117+mercuryVM@users.noreply.github.com>
2025-05-22 20:26:28 -0300

    feat(utils.py): adicionar função auxiliar update_gift_balance  (#84)
    * feat(utils): implement update_gift_balance function to manage gift balance
    
    * chore(tests): add balance test

--- src/api/utils.py
+++ src/api/utils.py
@@ -1,8 +1,7 @@
 import random
 import string
 from uuid import UUID
-
-from .models import Student
+from api.models import Gift, Student, StudentGift
 
 datetime_url_format = "%Y-%m-%dT%H:%M"
 
@@ -16,6 +15,13 @@ def generate_unique_code(length=3):
 
 def generate_token_code():
     return "".join(random.choices(string.ascii_uppercase + string.digits, k=5))
+
+def update_gift_balance (gift):
+    # Obtém o número de alunos que possuem esse Gift
+    students_count = StudentGift.objects.filter(gift=gift).count()
+    # Atualiza o saldo do Gift
+    gift.balance = students_count
+    gift.save()
 
 class UUIDConverter:
     regex = '[0-9a-fA-F-]+'  # Regex para UUID com hífens
