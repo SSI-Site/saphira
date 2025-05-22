37f84500038fdec7fb5426634a8e4ca7ba94f9ea
Caio Bernardo <98186360+caio-bernardo@users.noreply.github.com>
2025-05-22 17:21:00 -0300

    fix: half space of some field in Speaker model

--- src/api/models.py
+++ src/api/models.py
@@ -54,8 +54,8 @@ class StudentGift(models.Model):
 
 class Speaker(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=128)
-    description = models.CharField(max_length=1024, null=True)
-    social_media = models.CharField(max_length=128, null=True)
-    pronouns = models.CharField(max_length=32, null=True)
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=512, null=True)
+    social_media = models.CharField(max_length=64, null=True)
+    pronouns = models.CharField(max_length=16, null=True)
     role = models.CharField(max_length=64)
