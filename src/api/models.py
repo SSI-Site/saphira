ed53893b51cc5269d76f1dca5ac284fd8eb41148
Rafael Vinicius Nunes <rafaelviniciusnunes@gmail.com>
2025-06-04 10:32:40 -0300

    feat(models.py): cria o modelo e migração para DrawWinner (#94)
    * feat(models.py): cria modelo DrawWinner
    
    A classe DrawWinner apresenta duas chaves estrangeiras (student e talk) em relacionamento 1:1, com deleção em cascata (on_delete=models.CASCADE; quando student ou talk é deletado todos os DrawWinner associados também são)
    
    Co-authored-by: Douglas Macedo <macedodouglas52@gmail.com>
    
    * feat(migrations): cria DrawWinner no banco de dados
    
    Utiliza o comando "python manage.py makemigrations" para automaticamente gerar o arquivo de migração posteriormente utilizado através de "python manage.py migrate"
    
    Co-authored-by: Douglas Macedo <macedodouglas52@gmail.com>
    
    ---------
    
    Co-authored-by: Douglas Macedo <macedodouglas52@gmail.com>

--- src/api/models.py
+++ src/api/models.py
@@ -59,3 +59,11 @@ class Speaker(models.Model):
     social_media = models.CharField(max_length=64, null=True)
     pronouns = models.CharField(max_length=16, null=True)
     role = models.CharField(max_length=64)
+
+class DrawWinner(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    talk = models.ForeignKey(Talk, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('student', 'talk',)
