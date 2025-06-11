419b9f152b015d9c6a6be9f7af212aa58715d385
Gabriel Luís da Silva <officialmercurymusicdj@gmail.com>
2025-06-11 19:51:44 -0300

    fix(gifts): update query parameters in AdminListCreateGiftsView to use request.GET and not kwargs

--- src/api/views.py
+++ src/api/views.py
@@ -464,8 +464,8 @@ class AdminListCreateGiftsView(generics.ListCreateAPIView):
     
     def get(self, request, *args, **kwargs):
         queryset = Gift.objects.filter(
-            models.Q(id=kwargs.get('id')) |
-            models.Q(name=kwargs.get('name'))
+            models.Q(id=request.GET.get('id')) |
+            models.Q(name=request.GET.get('name'))
         )
         serializer = GiftSerializer(queryset, many=True)
         return Response(serializer.data)
