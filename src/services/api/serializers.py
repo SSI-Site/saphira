067b0fe7ee3881d18cfb35de1e3d8178e728af43
Rafael Vinicius Nunes <rafaelviniciusnunes@gmail.com>
2025-07-20 20:56:40 -0300

    feat(serializers.py): Adiciona o DrawWinnerSerializer para manipular os dados de DrawWinner
    O serializer é necessário para indicar ao Django Rest Framework o que deve ser ou não serializado e validado

--- src/services/api/serializers.py
+++ src/services/api/serializers.py
@@ -101,6 +101,17 @@ class GiftSerializer(serializers.ModelSerializer):
         )
         return gift
 
+class DrawWinnerSerializer(serializers.ModelSerializer):
+    student = serializers.PrimaryKeyRelatedField(queryset=Student.objects.all())
+    talk = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = DrawWinner
+        fields = ['id', 'talk', 'student']
+
+    def create(self, validated_data):
+        draw_winner = DrawWinner.objects.create(**validated_data)
+        return draw_winner
 
 class AdminSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
