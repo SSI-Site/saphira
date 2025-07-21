e681b1fab38ef708938d4250f5d75684eafde605
Rafael Vinicius Nunes <rafaelviniciusnunes@gmail.com>
2025-07-21 10:16:01 -0300

    feat: criar view AdminListCreateWinner #85 (#119)
    * feat(serializers.py): Adiciona o DrawWinnerSerializer para manipular os dados de DrawWinner
    
    O serializer é necessário para indicar ao Django Rest Framework o que deve ser ou não serializado e validado
    
    * feat(views.py): Adiciona método para listar os DrawWinners de uma Talk por url param
    
    Utiliza o DrawWinnerSerializer criado para manipular os dados e buscar os vencedores do sorteio a partir da id de Talk pela url. Os casos de exceção são tratados na própria view, fora do serializer.
    
    * feat(views.py): Adiciona método para criar um DrawWinner a partir de um Talk id por url param
    
    Também utiliza o DrawWinnerSerializer, mas mantém tratando as exceções no próprio método da view (validação da Talk, do Student, da Presence e do próprio DrawWinner).
    
    * feat(urls.py): Adiciona os novos métodos de AdminListCreateWinner para a rota /admin/winners/talks/<int:talk_id>
    
    A rota não acessa a Talk específica de um DrawWinner, na verdade ela busca os DrawWinners a partir da Talk id, diferente do que se esperaria pela convenção de rotas.
    
    * refactor(views.py): Resolve conflito de rotas padronizando o acesso por recursos da rota DrawWinners
    
    Anterior haviam duas rotas: /admin/winners para ler os DrawWinner e /admin/winners/<uuid:student_id> que removia o DrawWinner a partir do recurso específicoStudent. Com a implementação da issue #85 haveria conflito com a nova rota /admin/winners/<uuid:talk_id> que lista os ganhadores por palestra e cria novos ganhadores manualmente, de forma que precisou ser transformada em /admin/winners/talks/<uuid:talk_id> e /admin/winners/students/<uuid:student_id>, representando o acesso por recurso específico e mantendo um padrão para rotas conflitantes.
    
    * test(AdminListCreateWinner): Adiciona testes para os novos métodos de AdminListCreateWinner para validar corpos de resposta e requisições mal feitas
    
    Os testes cobrem todos os cenários possíveis pensados para uma má requisição na rota de busca e criação dos DrawWinners pelo recurso específico Student, são esses: Busca por dados inexistentes, atribuição de premio a alune que não foi na palestra e tentativa de atribuição de mais de um brinde a alune já registrade. Além disso verifica os body de resposta para os casos de criação e busca.
    
    * chore(views.py): Remoção de imports não utilizados
    
    Remove imports não utilizados no documento. O import de models (from .models import *) pois ele já é importado em serializers.py, tornando o import statement redundante.

--- src/services/api/urls.py
+++ src/services/api/urls.py
@@ -32,7 +32,8 @@ urlpatterns = [
     path('admin/gifts', AdminListCreateGiftsView.as_view(), name='admin-list-create-gifts'),
     path('admin/gifts/<uuid:id>', AdminUpdateDestroyGiftView.as_view(), name='admin-update-destroy-gift'),
     path('admin/winners', AdminListWinnerView.as_view(), name='admin-list-draw-winners'),
-    path('admin/winners/<uuid:student_id>', AdminDestroyWinnerView.as_view(), name='admin-destroy-winner'),
+    path('admin/winners/students/<uuid:student_id>', AdminDestroyWinnerView.as_view(), name='admin-destroy-winner'),
+    path('admin/winners/talks/<int:talk_id>', AdminListCreateWinner.as_view(), name='admin-list-create-winner'),
 
     path('admin/students/', include(admin_students_urls)),
     path('admin/student/', include(admin_student_urls)),
