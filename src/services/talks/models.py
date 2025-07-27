b824120774a3b1983f4ae72428b5f867bb8dffcd
lion <clion.pg@gmail.com>
2025-07-27 18:58:08 -0300

    feat: adicao do campo activity_type no model de Talk, atualizacao do seu serializer e migracao

--- src/services/talks/models.py
+++ src/services/talks/models.py
@@ -3,6 +3,11 @@ from django.db import models
 from services.speakers.models import Speaker
 
 # Create your models here.
+
+class TalkActivityType(models.TextChoices):
+    WORKSHOP = 'WS', 'Workshop'
+    PRESENTATION = 'PR', 'Presentation'
+
 class Talk(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=128, unique=True)
@@ -10,6 +15,7 @@ class Talk(models.Model):
     description = models.CharField(max_length=1024, null=True)
     start_time = models.DateTimeField(unique=True)
     end_time = models.DateTimeField(unique=True)
+    activity_type = models.CharField(max_length=2, choices=TalkActivityType.choices, default=TalkActivityType.PRESENTATION)
 
     def __str__(self) -> str:
         return f"Talk: '{self.title}'"
--- src/services/talks/serializers.py
+++ src/services/talks/serializers.py
@@ -2,12 +2,13 @@ from rest_framework import serializers
 
 from services.api.utils import datetime_url_format
 from services.speakers.models import Speaker
-from .models import Talk
+from .models import Talk, TalkActivityType
 
 class TalkSerializer(serializers.ModelSerializer):
     start_time = serializers.DateTimeField(format=datetime_url_format, input_formats=[datetime_url_format])
     end_time = serializers.DateTimeField(format=datetime_url_format, input_formats=[datetime_url_format])
     speakers = serializers.PrimaryKeyRelatedField(many=True, queryset=Speaker.objects.all())
+    activity_type = serializers.ChoiceField(choices=TalkActivityType.choices, default=TalkActivityType.PRESENTATION)
 
     class Meta:
         model = Talk
