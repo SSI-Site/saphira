4bc5351959aec08d43d153494824180c11fd8288
lion <clion.pg@gmail.com>
2025-07-27 21:06:05 -0300

    feat: adicao do campo sponsor dentro do model de Talk, atualizacao do serializer e migracao

--- src/services/talks/models.py
+++ src/services/talks/models.py
@@ -3,6 +3,12 @@ from django.db import models
 from services.speakers.models import Speaker
 
 # Create your models here.
+class Sponsor(models.Model):
+    name = models.CharField(max_length=64)
+    url = models.URLField()
+
+    def __str__(self):
+        return f"Sponsor: '{self.name}'"
 
 class TalkActivityType(models.TextChoices):
     WORKSHOP = 'WS', 'Workshop'
@@ -16,6 +22,7 @@ class Talk(models.Model):
     start_time = models.DateTimeField(unique=True)
     end_time = models.DateTimeField(unique=True)
     activity_type = models.CharField(max_length=2, choices=TalkActivityType.choices, default=TalkActivityType.PRESENTATION)
+    sponsor = models.ForeignKey(Sponsor, on_delete=models.SET_NULL, null=True, blank=True, related_name='talks')
 
     def __str__(self) -> str:
         return f"Talk: '{self.title}'"
