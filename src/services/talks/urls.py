391b7bd8c17ce124a607c7bea8b1052ab300d146
Caio Bernardo <caio.2004vb@gmail.com>
2025-07-28 16:50:36 -0300

    fix: arrum import em talks

--- src/services/talks/urls.py
+++ src/services/talks/urls.py
@@ -4,7 +4,8 @@ from .views import (
     AdminListCreateTalksView,
     AdminRetrieveUpdateDestroyTalkView,
     AdminListCreateSponsorView,
-    AdminRetrieveUpdateDestroySponsorView
+    AdminRetrieveUpdateDestroySponsorView,
+    ListRetrieveTalksView
 )
 
 talks_urls = [
