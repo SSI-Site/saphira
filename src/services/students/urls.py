76f6ee5d3aafb35272687e56bbaac2ca43a42108
EduardoXDuardo <luiz.e.silva@usp.br>
2025-07-28 11:07:44 -0300

    fix: corrige sintaxe da URL em admin_student_urls
    Altera {uuid:student_id} para <uuid:student_id> na rota de admin student gifts

--- src/services/students/urls.py
+++ src/services/students/urls.py
@@ -18,7 +18,7 @@ admin_students_urls = [
 
 admin_student_urls = [
     path('<student_document>', views.AdminRetrieveStudentInfoView.as_view(), name='admin-retrieve-student-info'),
-    path('{uuid:student_id}/gifts', views.AdminListRetrieveStudentGiftsByStudentView.as_view(), name='admin-list-retrieve-student-gifts'),
+    path('<uuid:student_id>/gifts', views.AdminListRetrieveStudentGiftsByStudentView.as_view(), name='admin-list-retrieve-student-gifts'),
 ]
 
 urlpatterns = student_urls
