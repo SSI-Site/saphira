abd0b3d453a940578e8dd296b82f7136451d4d33
lion <clion.pg@gmail.com>
2025-07-30 00:29:03 -0300

    refactor: remocao do campo sponsor type (#150)

--- src/services/talks/serializers.py
+++ src/services/talks/serializers.py
@@ -2,13 +2,12 @@ from rest_framework import serializers
 
 from services.api.utils import datetime_url_format
 from services.speakers.models import Speaker
-from .models import Talk, TalkActivityType, Sponsor, SponsorType
+from .models import Talk, TalkActivityType, Sponsor
 
 class SponsorSerializer(serializers.ModelSerializer):
-    sponsor_type = serializers.ChoiceField(choices=SponsorType.choices, default=SponsorType.SPONSOR)
     class Meta:
         model = Sponsor
-        fields = ['id', 'name', 'url', 'sponsor_type']
+        fields = ['id', 'name', 'url']
 
 class TalkSerializer(serializers.ModelSerializer):
     start_time = serializers.DateTimeField(format=datetime_url_format, input_formats=[datetime_url_format])
--- tests/test_adminRetrieveUpdateDestroySponsorView.py
+++ tests/test_adminRetrieveUpdateDestroySponsorView.py
@@ -3,8 +3,7 @@ from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 from rest_framework import status
 from django.urls import reverse
-
-from services.talks.models import Sponsor,SponsorType, Talk
+from services.talks.models import Sponsor, Talk
 from services.speakers.models import Speaker
 from datetime import datetime as dt, timedelta
 from zoneinfo import ZoneInfo
@@ -30,30 +29,27 @@ class AdminRetrieveUpdateDestroySponsorViewTestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["name"], self.sponsor.name)
         self.assertEqual(response.data["url"], self.sponsor.url)
-        self.assertEqual(response.data["sponsor_type"], SponsorType.SPONSOR)
 
     def test_update_sponsor(self):
         updated_data = {
             "name": "Sponsor Atualizado",
-            "url": "https://atualizado.com",
-            "sponsor_type": SponsorType.PARTNER
+            "url": "https://atualizado.com"
         }
         response = self.client.put(self.url, updated_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["message"], "Sponsor atualizado com sucesso.")
-        self.assertEqual(response.data["sponsor"]["sponsor_type"], SponsorType.PARTNER)
+        self.assertEqual(response.data["sponsor"]["name"], updated_data["name"])
+        self.assertEqual(response.data["sponsor"]["url"], updated_data["url"])
         self.sponsor.refresh_from_db()
         self.assertEqual(self.sponsor.name, updated_data["name"])
-        self.assertEqual(self.sponsor.sponsor_type, SponsorType.PARTNER)
+        self.assertEqual(self.sponsor.url, updated_data["url"])
 
     def test_partial_update_sponsor(self):
-        response = self.client.patch(self.url, {"url": "https://newurl.com", "sponsor_type": SponsorType.PARTNER}, format='json')
+        response = self.client.patch(self.url, {"url": "https://newurl.com"}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["sponsor"]["url"], "https://newurl.com")
-        self.assertEqual(response.data["sponsor"]["sponsor_type"], SponsorType.PARTNER)
         self.sponsor.refresh_from_db()
         self.assertEqual(self.sponsor.url, "https://newurl.com")
-        self.assertEqual(self.sponsor.sponsor_type, SponsorType.PARTNER)
 
     def test_update_sponsor_invalid_data(self):
         response = self.client.put(self.url, {"name": "", "url": "invalid"}, format='json')
